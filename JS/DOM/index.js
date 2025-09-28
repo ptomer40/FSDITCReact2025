e67db8eb42d06c1b4b1e47ab9dce46d5d6280974
ptomer40 <ptomer40@yahoo.com>
2025-09-28 11:14:37 +0530

    callback and promise completed

---
+++ JS/DOM/index.js
@@ -0,0 +1,106 @@
+// const div=document.getElementsByTagName('div');
+
+// div[0].innerText="Hello World";
+// div[0].style.color='red';
+// div[0].style.backgroundColor='cyan';
+// console.log(div);
+
+//const container=document.getElementsByClassName('container');
+
+// container[0].innerHTML='<h2 style=color:red>Hello World</h2>';
+// console.dir(container);
+// const h2=document.createElement('h2');
+// h2.innerText="ABES Engineering College";
+// h2.style.color="white";
+// h2.style.backgroundColor='brown';
+// console.log(h2);
+// container[0].appendChild(h2);
+
+
+
+
+// const button=document.getElementById('btn');
+// console.log(button)
+
+ 
+// function msg(){
+//     document.getElementById('disp').innerHTML="<h>Loading immage....</h>";
+//     setTimeout(()=>{
+//       const img=document.createElement('img');
+// img.src='https://fastly.picsum.photos/id/0/5000/3333.jpg?hmac=_j6ghY5fCfSD6tvtcV74zXivkJSPIfR9B8w34XeQmvU';
+// img.setAttribute('height','200px');
+// img.setAttribute('width','200px');
+// console.log(img);
+// document.getElementById('disp').innerHTML="<h2>Welcome to ABES Engineering College</h2>"
+   
+// container[0].appendChild(img);
+//     },2000);
+   
+     //console.log("Hi, I m working on DOM");
+    //alert("Hii, I m using DOM");
+
+//}
+//button.addEventListener('click',msg);
+
+
+// Blocking and Non-blocking Code
+
+// const button=document.getElementById('btn');
+// function longData(){
+//     console.log("Start");
+//     for(i=0;i<100000000;i++){
+//         console.log(i);
+//     }
+//     console.log("End");
+// }
+// button.addEventListener('click',longData);
+
+// console.log("start")
+// setTimeout(()=>{
+// console.log("Welcome to FSD")
+// },2000)
+// console.log("end")
+
+//Callback function
+
+// function greetmsg(myname){
+//     return "Hii,"+myname+" Welcome to abes College";
+// }
+
+// function msgHandler(msg,clbk){
+//     //console.log(msg);
+//     const data=clbk(msg);
+//     return data;
+// }
+
+// const student=["Rahul","Aman","Vansh"];
+// student.forEach((name)=>{
+// console.log(msgHandler(name,greetmsg))
+// })
+// const completemsg=msgHandler("Hiii",greetmsg);
+//console.log(completemsg);
+
+//Promise
+
+
+const mypromise=new Promise((resolve,reject)=>{
+const password="7868gh#$0";
+if(password.length>8){
+    resolve("Password length is ok");
+}else{
+    reject("Password length is not as per our policy");
+}
+});
+
+// mypromise.then((result)=>{console.log(result)})
+// .catch((err)=>{console.log(err)})
+// .finally(()=>{
+//     console.log("All the resources have closed successfully");
+// })
+
+
+async function handleData(){
+         const data= await mypromise;
+          console.log(data)   
+}
+handleData();
